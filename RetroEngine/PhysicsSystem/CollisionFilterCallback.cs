29bfe467c8ea35e65e1b3c4cb658fb8e84f61eac
BitKovin <39824091+BitKovin@users.noreply.github.com>
2025-01-20 15:15:30 +0200

    maybe optimized collision filter

--- RetroEngine/PhysicsSystem/CollisionFilterCallback.cs
+++ RetroEngine/PhysicsSystem/CollisionFilterCallback.cs
@@ -13,15 +13,20 @@ namespace RetroEngine.PhysicsSystem
     internal class CollisionFilterCallback : OverlapFilterCallback
     {
 
+        internal static Dictionary<BroadphaseProxy, CollisionObject> savedValues = new Dictionary<BroadphaseProxy, CollisionObject>();
+        internal static HashSet<BroadphaseProxy> savedProxies = new HashSet<BroadphaseProxy>();
+
         public override bool NeedBroadphaseCollision(BroadphaseProxy proxy0, BroadphaseProxy proxy1)
         {
 
             if(proxy0==null || proxy1 == null)
                 return false;
 
+
+
             // Retrieve user indices from collision objects
-            var colObj0 = (CollisionObject)proxy0.ClientObject;
-            var colObj1 = (CollisionObject)proxy1.ClientObject;
+            var colObj0 = GetCollisionObjectFromProxy(proxy0);
+            var colObj1 = GetCollisionObjectFromProxy(proxy1);
 
             if (colObj0 == null || colObj1 == null)
                 return false;
@@ -41,10 +46,29 @@ namespace RetroEngine.PhysicsSystem
             return test1 && test2;
         }
 
+        CollisionObject GetCollisionObjectFromProxy(BroadphaseProxy proxy)
+        {
+
+            if(savedProxies.Contains(proxy)) return savedValues[proxy];
+
+            savedValues.TryAdd(proxy, (CollisionObject)proxy.ClientObject);
+            savedProxies.Add(proxy);
+            return savedValues[proxy];
+
+        }
+
         private bool ShouldCollide(BodyType collidesWith, BodyType bodyType)
         {
             // Use bitwise AND to check if any flag is set in both collidesWith and bodyType
             return (collidesWith & bodyType) != 0;
+        }
+
+        internal static void ResetCache()
+        {
+            savedValues.Clear();
+            savedValues.EnsureCapacity(10000);
+            savedProxies.Clear();
+            savedProxies.EnsureCapacity(10000);
         }
 
     }
